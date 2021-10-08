33d7504b248ed430d53d42d6d0b5d18043bd4567
Rob Hanssen <rob.hanssen@outlook.com>
2021-10-07 21:25:11 -0400

    updated gdp chart

--- gdpratio.r
+++ gdpratio.r
@@ -15,6 +15,19 @@ gdp <- read_csv("gdp.csv") %>%
         mutate(date = as_datetime(date)) %>%
         rename(floordate = "date")
 
+gdp %>% 
+        filter(floordate > as_datetime("1979-12-31")) %>%
+        ggplot() + 
+        aes(floordate, gdp) + 
+        scale_y_log10(labels = scales::dollar_format(scale = 1/1000, suffix = "T"),
+                        breaks = rep(c(1, 2, 5), times = 3) * rep(10^(3:6), each = 3)) + 
+        geom_line() +
+        theme_light() + 
+        geom_vline(xintercept = inaugdates, lty = 2, alpha = .3) +
+        labs(x = "Date", y = "US GDP in ($T)", title = "US GDP since 1980")
+
+ggsave("us-gsp-since-1980.png", width = 6, height = 6)
+
 # small fudge to cram in the last date point. Assumption is GDP growth of 500 B
 gdpadd <- tibble(floordate = as_datetime("2021-07-01"), gdp = last(gdp$gdp)+500)
 gdp <- bind_rows(gdp, gdpadd)
