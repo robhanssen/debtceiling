aa5724ba983a3707b12a772744806c3f5c455bb9
Rob Hanssen <rob.hanssen@outlook.com>
2021-10-11 09:44:51 -0400

    updates

--- debtceiling.r
+++ debtceiling.r
@@ -3,8 +3,20 @@ library(broom)
 library(lubridate)
 theme_set(theme_light())
 
-inaugdates = sort(as_datetime(as.Date(c("1900-01-01","2100-01-01","2009-01-20","2017-01-21","2001-01-21","1993-01-21", "1989-01-21", "1981-01-21"))))
-presidents = c("Before 1980", "Reagan", "Bush Sr", "Clinton", "Bush Jr", "Obama", "Trump" )
+secondsperyear <- 365 * 24 *3600
+partycolor <- c("R" = "red", "D" = "blue")
+
+presidentinfo <-
+    read_csv("presidents.csv") %>%
+    mutate(inaugdate = as_datetime(inaugdate),
+           party = factor(party, levels = c("R","D"))
+           )
+
+inaugdates <- c(as_datetime("1776-03-31"),
+                presidentinfo$inaugdate,
+                as_datetime("9999-12-31"))
+
+presidents <- c("", presidentinfo$president)
 
 debt <- read_csv("debtceiling.csv") %>%
         janitor::clean_names() %>%
@@ -47,7 +59,8 @@ debtmodelinfo <-
     mutate(info = map(model, tidy)) %>%
     unnest(info) %>% 
     filter(term == "date") %>%
-    mutate(growth = estimate * 365 * 24 * 60 * 60)
+    mutate(growth = estimate * secondsperyear) %>%
+    inner_join(presidentinfo, by = "president")
 
 
 # alternate model
@@ -64,27 +77,41 @@ altmodel <-
 
 altmodel2 <-
     debt %>%
-    filter(date > as_datetime("1981-12-31")) %>%
-    mutate(pres2 = ifelse(date < as_datetime("1991-12-31"), "Group 1", "Group 2")) %>%
+    filter(date > as_datetime("1980-12-31")) %>%
+    mutate(pres2 = ifelse(president == "Reagan" | president == "GHW Bush", "Group 1", "Group 2")) %>%
     mutate(pres2 = factor(pres2)) %>%
     lm(log10(debt_ceiling) ~ date * pres2, data = .) %>% 
     augment() %>%
     mutate(debt_ceiling = 10^.fitted)
     
 debtdata %>%
-    filter(president != "Before 1980") %>%
+    filter(president != "") %>%
     group_by(president) %>%
     ggplot() + 
     aes(date, debt_ceiling, color = president) + 
     geom_line() + 
     scale_y_log10(labels = scales::dollar_format(scale = 1/1000, suffix = "T")) + 
     geom_vline(xintercept =  inaugdates, lty = 2, alpha = .4) + 
-    geom_point(data = debt %>% filter(president != "Before 1980")) + 
+    geom_point(data = debt %>% filter(president != "")) + 
     labs(x = "Date", y = "Debt ceiling (in $T)", color = "President",
          title = "Increase in the debt ceiling since 1980", 
          caption = "Dotted vertical lines are presidential inauguration dates") + 
     #geom_line(data = altmodel, aes(color = pres2), lty = 2) +
     theme(legend.position = "none") +
-    geom_line(data = altmodel2, aes(color = NA), color = "black", lty = 2)
+    geom_line(data = altmodel2, aes(color = NA), color = "black", lty = 2, size = 1.2)
 
     ggsave("debtceiling-since-1980.png", width = 6, height = 6)
+
+debtmodelinfo %>%
+    ggplot +
+    aes(x = fct_reorder(president, desc(inaugdate)), y = growth, fill = party) + 
+    geom_col() + 
+    scale_y_continuous(labels =  scales::percent_format(accuracy = .1)) + 
+    scale_fill_manual(values = partycolor) + 
+    labs(x = "President",
+         y = "Debt ceiling CAGR (in %)",
+         fill = "Party", 
+         title = "US debt ceiling growth since the 1960s") + 
+    coord_flip()
+
+ggsave("debtceiling-growth-by-president.png", width = 6, height = 8)
