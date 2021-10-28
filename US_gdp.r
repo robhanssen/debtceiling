1054fcbd5ed20fec2d26b36d217f666910a882fd
Rob Hanssen <rob.hanssen@outlook.com>
2021-10-28 15:06:58 -0400

    gdp plot

--- US_gdp.r
+++ US_gdp.r
@@ -1,11 +1,6 @@
 source("std/stdpackages.r")
 source("std/stdfunctions.r")
 
-debt <- read_csv("sources/publicdebt.csv") %>%
-        janitor::clean_names() %>%
-        mutate(date = as_datetime(date)) %>%
-        rename(floordate = "date")
-
 gdp <- read_csv("sources/gdp.csv") %>%
         janitor::clean_names() %>%
         mutate(date = as_datetime(date)) %>%
@@ -18,25 +13,25 @@ presidentinfo <-
         rename(floordate = "date")
 
 
-debt <-
-    debt %>%
+gdp_plot <-
+    gdp %>%
         mutate(floordate = floor_date(floordate, unit = "quarter")) %>%
         # mutate(date = as_datetime(date),
         #         floordate = as_datetime(floordate)) %>%
-        filter(!is.na(debt)) %>%
-        inner_join(gdp) %>%
-        mutate(gdpratio = gfdebtn / (1000*gdp)) %>%
+        # filter(!is.na(debt)) %>%
+        # inner_join(gdp) %>%
+        # mutate(gdpratio = gfdebtn / (1000*gdp)) %>%
         left_join(presidentinfo) %>% 
         fill(party, .direction = "down")
 
-debt %>%
+gdp_plot %>%
     filter(floordate > as_datetime("1979-12-31")) %>%
     ggplot() +
-    aes(floordate, gdpratio, color = party, group = FALSE) +
+    aes(floordate, gdp, color = party, group = FALSE) +
     geom_line() +
-    scale_y_continuous(breaks = .2 * 0:100,
-                        limits = c(0, NA),
-                        labels = scales::percent_format()) +
+    scale_y_log10(
+                        limits = c(3000, NA),
+                        labels = scales::dollar_format()) +
     geom_vline(xintercept = inaugdates, lty = 2, alpha = .3) +
     theme_light() +
     labs(x = "Date",
@@ -46,4 +41,4 @@ debt %>%
     scale_color_manual(values = partycolor) + 
     theme(legend.position = "none")
 
-ggsave("graphs/debt-to-gdp-ratio-since-1980.png", width = 6, height = 6)
+# ggsave("graphs/debt-to-gdp-ratio-since-1980.png", width = 6, height = 6)
